dd37137e501ad9f4a232d8396dc9ffa8d3ff55b0
Kyle Kutchek <kyle.kutchek@gmail.com>
2022-12-05 22:03:52 -0800

    ezpz

---
+++ 2022/Day06/6B.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+from pathlib import Path
+
+p = Path(__file__).with_name("input.txt")
+with p.open("r") as file:
+    signal = file.read()
+
+start_marker_size = 14
+
+for index in range(len(signal) - start_marker_size):
+    if len(set(signal[index:index+start_marker_size])) == start_marker_size:
+        break
+
+print(index + start_marker_size)
